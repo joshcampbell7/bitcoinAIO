618a935d2b079f5298b39c5cbd43667cebe190f8
Joshua Campbell <jebc1@student.le.ac.uk>
2025-02-20 23:20:34 +0000

    init

---
+++ bitcoinAIO/src/main/java/com/bitcoin/bitcoinaio/BitcoinAioApplication.java
@@ -0,0 +1,13 @@
+package com.bitcoin.bitcoinaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinAioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitcoinAioApplication.class, args);
+	}
+
+}
---
+++ bitcoinAIO/src/test/java/com/bitcoin/bitcoinaio/BitcoinAioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitcoin.bitcoinaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinAioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
